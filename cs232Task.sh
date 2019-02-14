fe3780ed53aa40bec619f2dd07014f431ebf23c5
NikhilMTomy <nikhilmtomy@gmail.com>
2019-02-14 12:03:04 +0530

    updated

--- cs232Task.sh
+++ cs232Task.sh
@@ -3,3 +3,5 @@ pdftotext -nopgbrk result.pdf
 grep --no-group-separator -A3 'CHN17CS' result.txt > result_CS_Only.txt
 tr '\n' ' ' < result_CS_Only.txt > result_no_newline
 sed 's/\ CHN/\nCHN/g' result_no_newline > result_one_per_line
+curl -fLo "s4d_student_details" "http://192.168.0.2/s4d/s4d.txt"
+grep "CHN" s4d_student_details | awk '{s=""; for (i=3; i<=NF; i++) s=s $i " "; print s }' | sed '/^L.*/d' > s4d_student_registor_number_and_name
